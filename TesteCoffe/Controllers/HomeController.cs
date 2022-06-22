5ffc9b8a4af21b70094fb252abdaf398900ec608
ttmaha <tittongato001>
2022-06-22 13:09:19 -0300

    Adicionar arquivos de projeto.

---
+++ TesteCoffe/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TesteCoffe.Models;
+
+namespace TesteCoffe.Controllers
+{
+    public class HomeController : Controller
+    {
+  
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+        public IActionResult Tattoos()
+        {
+            return View();
+        }
+
+        public IActionResult Loja()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Produtos()
+        {
+            return View();
+        }
+
+
+    }
+}
